0b881349516ad0fde64cb315fa65ac215f07371f
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-10-16 15:24:54 +0100

    file database in json

--- helpers.go
+++ helpers.go
@@ -4,20 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strings"
 )
-
-func getCleanedBody(body string, badWords map[string]struct{}) string {
-	words := strings.Split(body, " ")
-	for i, word := range words {
-		loweredWord := strings.ToLower(word)
-		if _, ok := badWords[loweredWord]; ok {
-			words[i] = "****"
-		}
-	}
-	cleaned := strings.Join(words, " ")
-	return cleaned
-}
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
