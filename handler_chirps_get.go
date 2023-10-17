267da0f8457dfb04aae9c7c25fa9a5b226746d56
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-10-17 19:04:27 +0100

    db file manipulation

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,26 +3,58 @@ package main
 import (
 	"net/http"
 	"sort"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.DB.GetChirps()
+	author_search := r.URL.Query().Get("author_id")
+	sortBy := r.URL.Query().Get("sort")
+	if sortBy == "" {
+		sortBy = "asc"
+	}
+
+	dbChirps, err := cfg.DB.GetChirps(&author_search)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
+		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		chirps = append(chirps, Chirp{
-			ID:   dbChirp.ID,
-			Body: dbChirp.Body,
+			ID:       dbChirp.ID,
+			Body:     dbChirp.Body,
+			AuthorID: dbChirp.AuthorID,
 		})
 	}
 
 	sort.Slice(chirps, func(i, j int) bool {
+		if sortBy == "desc" {
+			return chirps[i].ID > chirps[j].ID
+		}
+		//asc
 		return chirps[i].ID < chirps[j].ID
 	})
 
 	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) handlerChirp(w http.ResponseWriter, r *http.Request) {
+	chirpID := chi.URLParam(r, "chirpID")
+	idChirp, err := strconv.Atoi(chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirp")
+		return
+	}
+	dbChirp, err := cfg.DB.GetChirp(idChirp)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Chirp not found")
+		return
+	}
+
+	chirp := dbChirp
+
+	respondWithJSON(w, http.StatusOK, chirp)
 }
--- main.go
+++ main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jhonipereira/chirpy/internal/database"
+	"github.com/joho/godotenv"
 )
 
 type apiConfig struct {
 	fileserverHits int
-	DB             *database
+	DB             *database.DB
+	jwtSecret      string
+	polkaKey       string
+}
+
+var debugFlag bool
+
+func init() {
+	flag.BoolVar(&debugFlag, "debug", false, "Enable debug mode")
+	flag.Parse()
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	if debugFlag {
+		// Enable debug mode
+		log.Println("Debug mode is enabled")
+		// Check if the file exists
+		if _, err := os.Stat("database.json"); err == nil {
+			// Delete the file
+			err := os.Remove("database.json")
+			if err != nil {
+				log.Println("Error deleting the file:", err)
+			}
+		}
+	}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	jwtSecret := os.Getenv("JWT_SECRET")
+	polkaKey := os.Getenv("POLKA_KEY")
 
 	db, err := database.NewDB("database.json")
 	if err != nil {
@@ -26,6 +58,8 @@ func main() {
 	apiConfig := apiConfig{
 		fileserverHits: 0,
 		DB:             db,
+		jwtSecret:      jwtSecret,
+		polkaKey:       polkaKey,
 	}
 
 	r := chi.NewRouter()
@@ -41,6 +75,16 @@ func main() {
 	apiRouter.Get("/healthz", handleServerHealth)
 	apiRouter.Post("/chirps", apiConfig.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiConfig.handlerChirpsRetrieve)
+	apiRouter.Get("/chirps/{chirpID}", apiConfig.handlerChirp)
+	apiRouter.Delete("/chirps/{chirpID}", apiConfig.handlerChirpsDelete)
+
+	apiRouter.Post("/polka/webhooks", apiConfig.handlerPolkaWebhook)
+
+	apiRouter.Post("/revoke", apiConfig.handlerRevoke)
+	apiRouter.Post("/refresh", apiConfig.handlerRefresh)
+	apiRouter.Post("/users", apiConfig.handlerUsersCreate)
+	apiRouter.Put("/users", apiConfig.handlerUsersUpdate)
+	apiRouter.Post("/login", apiConfig.handlerUsersLogin)
 
 	r.Mount("/api", apiRouter)
 
